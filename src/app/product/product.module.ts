5f645650c295e73f3800f45c72b6aed748939bf3
Vladzlaf <vladzlafm@gmail.com>
2023-08-16 15:59:10 +0300

    delete: postgres-data

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,9 +1,19 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 
+import { ProductEntity } from './entities/product.entity';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { ProductRepo } from './repo/product.repo';
+
+// Импорт DTO
 
 @Module({
+  imports: [
+    MikroOrmModule.forFeature({
+      entities: [ProductEntity, ProductRepo],
+    }),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
