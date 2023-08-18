bb0ca5d19f9f4d8af4091412645d719c9362c752
Vlad <94391054+Vladzlaf@users.noreply.github.com>
2023-08-18 16:40:42 +0300

    Update product.entity.ts with new field: image_url, and moved fields: quantity and price

--- src/shared/entities/product.entity.ts
+++ src/shared/entities/product.entity.ts
@@ -12,8 +12,17 @@ export class ProductEntity extends UUIDEntity {
   @Property({ name: 'name' })
   name!: string;
 
+  @Property({ name: 'price' })
+  price!: number;
+
   @Property({ name: 'description' })
   description!: string;
+
+  @Property({ name: 'image_url' })
+  image_url!: string;
+
+  @Property({ name: 'quantity' })
+  quantity!: number;
 
   @Enum({ name: 'category', array: false, items: () => ProductCategory })
   category!: ProductCategory;
