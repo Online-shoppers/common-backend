5895184f425c9713313082e70120a00c0de93760
Vladzlaf <vladzlafm@gmail.com>
2023-08-21 16:01:11 +0300

    feat: added dto field beer, snacks, accessoty from base dto to method fromEntity

---
+++ src/migrations/Migration20230821115259.ts
@@ -0,0 +1,21 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230821115259 extends Migration {
+  async up(): Promise<void> {
+    this.addSql(
+      'alter table "products" alter column "category" type text[] using ("category"::text[]);',
+    );
+    this.addSql(
+      'alter table "products" alter column "type" type text[] using ("type"::text[]);',
+    );
+  }
+
+  async down(): Promise<void> {
+    this.addSql(
+      'alter table "products" alter column "category" type text[] using ("category"::text[]);',
+    );
+    this.addSql(
+      'alter table "products" alter column "type" type text[] using ("type"::text[]);',
+    );
+  }
+}
