800efc2789ccd74d92fd6255154dbff5b1e02421
ivanm <ivanmatsevich1@gmail.con>
2023-09-05 17:11:53 +0300

    feat: test user , userRoles controllers

--- src/app/user-roles/user-roles.controller.spec.ts
+++ src/app/user-roles/user-roles.controller.spec.ts
@@ -1,6 +1,10 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { JwtPermissionsGuard } from '../security/guards/jwt-permission.guard';
+import { UserService } from '../user/user.service';
 import { NewUserRoleForm } from './dto/new-user-role.form';
+import { UserRoleDto } from './dto/user-role.dto';
 import { UserPermissions } from './enums/user-permissions.enum';
 import { UserRoles } from './enums/user-roles.enum';
 import { UserRolesController } from './user-roles.controller';
@@ -18,19 +22,62 @@ const mockUserRoleService = {
     };
   }),
 };
+
+const userRoleDto: UserRoleDto[] = [
+  {
+    id: '1',
+    created: new Date().getDate(),
+    updated: new Date().getDate(),
+    type: UserRoles.Guest,
+    name: 'Guest',
+    permissions: [UserPermissions.GetUsers],
+    isDefault: true,
+  },
+];
 describe('UserRolesController', () => {
   let controller: UserRolesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserRolesController],
-      providers: [UserRolesService],
-    }).compile();
+      providers: [
+        UserRolesService,
+        {
+          provide: JwtPermissionsGuard,
+          useValue: jest.fn().mockImplementation(() => true),
+        },
+      ],
+    })
+      .overrideProvider(UserRolesService)
+      .useValue(mockUserRoleService)
+      .compile();
 
     controller = module.get<UserRolesController>(UserRolesController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  describe('controller method', () => {
+    it('should return UserRoleDto', async () => {
+      await expect(controller.addRoles(userRoleDto)).resolves.toEqual({
+        id: '1',
+        created: new Date().getDate(),
+        updated: new Date().getDate(),
+        type: UserRoles.Client,
+        permissions: [UserPermissions.GetUsers],
+        isDefault: true,
+      });
+    });
+
+    it('should return error', async () => {
+      try {
+        await mockUserRoleService.addRole();
+      } catch (e) {
+        await expect(controller.addRoles(userRoleDto)).toBeInstanceOf(
+          BadRequestException,
+        );
+      }
+    });
   });
 });
