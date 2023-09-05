2f2e925ac3e3ea8cb8e982e2027b18a295a8ed69
ivanm <ivanmatsevich1@gmail.con>
2023-09-05 16:15:33 +0300

    feat: test user , userRoles controllers

--- src/app/user-roles/user-roles.controller.spec.ts
+++ src/app/user-roles/user-roles.controller.spec.ts
@@ -1,8 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { NewUserRoleForm } from './dto/new-user-role.form';
+import { UserPermissions } from './enums/user-permissions.enum';
+import { UserRoles } from './enums/user-roles.enum';
 import { UserRolesController } from './user-roles.controller';
 import { UserRolesService } from './user-roles.service';
 
+const mockUserRoleService = {
+  addRole: jest.fn().mockImplementation((dto: NewUserRoleForm) => {
+    return {
+      id: '1',
+      created: new Date().getDate(),
+      updated: new Date().getDate(),
+      type: UserRoles.Client,
+      permissions: [UserPermissions.GetUsers],
+      isDefault: true,
+    };
+  }),
+};
 describe('UserRolesController', () => {
   let controller: UserRolesController;
 
